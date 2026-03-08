b10b67f292a0173b35d866ce8bf03078de8f3892
Darkoo04 <baptiste.hesnard@proton.me>
2026-03-08 15:22:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
 import { Youtube, Play, Loader2 } from 'lucide-react'
 import {
-  SiX, SiInstagram, SiTiktok, SiYoutube, SiGithub, SiGitlab, SiLinkedin,
+  SiX, SiInstagram, SiTiktok, SiYoutube, SiGithub, SiGitlab, SiLinkedinIN,
   SiFacebook, SiTwitch, SiDribbble, SiMedium, SiDevdotto, SiReddit,
   SiPinterest, SiThreads, SiBluesky, SiMastodon, SiSubstack, SiPatreon,
   SiKofi, SiBuymeacoffee, SiSnapchat, SiDiscord, SiTelegram, SiWhatsapp,
@@ -58,7 +58,7 @@ const SOCIAL_PLATFORMS: Record<string, { icon: IconType | LucideIcon; brandColor
   youtube: { icon: SiYoutube, brandColor: '#FF0000', buildUrl: (h) => `https://youtube.com/@${h}` },
   github: { icon: SiGithub, brandColor: '#181717', buildUrl: (h) => `https://github.com/${h}` },
   gitlab: { icon: SiGitlab, brandColor: '#FC6D26', buildUrl: (h) => `https://gitlab.com/${h}` },
-  linkedin: { icon: SiLinkedin, brandColor: '#0A66C2', buildUrl: (h) => `https://linkedin.com/in/${h}` },
+  linkedin: { icon: SiLinkedinIN, brandColor: '#0A66C2', buildUrl: (h) => `https://linkedin.com/in/${h}` },
   facebook: { icon: SiFacebook, brandColor: '#1877F2', buildUrl: (h) => `https://facebook.com/${h}` },
   twitch: { icon: SiTwitch, brandColor: '#9146FF', buildUrl: (h) => `https://twitch.tv/${h}` },
   dribbble: { icon: SiDribbble, brandColor: '#EA4C89', buildUrl: (h) => `https://dribbble.com/${h}` },
